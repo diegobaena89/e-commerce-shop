7e163689dc800d38f2f8e05c4dda815bf71659de
diegobaena89 <diegobf.cello@gmail.com>
2023-02-08 20:25:15 -0300

    feature: create HomePage main structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { globalStyles } from '../styles/global';
 import logoImg from '../assets/logo.svg';
 import { Container, Header } from '../styles/pages/app';
 
+import Image from 'next/image';
+
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
 
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt='' />
+        <Image src={logoImg} alt='' />
       </Header>
       <Component {...pageProps} />
     </Container>
