3f23823ced0015271d662dbc44cd0170765fcdcf
diegobaena89 <diegobf.cello@gmail.com>
2023-02-10 18:06:52 -0300

    feature: Creating product structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,25 @@
 import { useRouter } from 'next/router';
+import { ImageContainer, ProductContainer, ProductDetails } from '../product';
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>Product {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quisquam
+          fugiat suscipit voluptatibus quis, rerum repellat, sed odio tenetur
+          illo doloribus fuga unde nisi veritatis facilis rem sit eveniet
+          laborum.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
