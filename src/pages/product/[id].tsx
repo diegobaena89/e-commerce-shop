cf48af34dcee0de6a62104b6ff84d04bab132003
diegobaena89 <diegobf.cello@gmail.com>
2023-02-14 19:02:59 -0300

    feature: purchase button

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,9 @@
 import { stripe } from '@/src/lib/stripe';
+import axios from 'axios';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import Stripe from 'stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '../product';
 
@@ -17,8 +19,25 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
-  function handleByProduct() {
-    console.log(product.defaultPriceId);
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+
+  async function handleByProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      setIsCreatingCheckoutSession(false);
+
+      alert('Falha ao redirecionar ao checkout');
+    }
   }
 
   const { isFallback } = useRouter();
@@ -39,7 +58,9 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button onClick={handleByProduct}>Comprar agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleByProduct}>
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   );
