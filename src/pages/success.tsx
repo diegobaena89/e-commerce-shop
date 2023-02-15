f0b654d621f68e64abfe1d1f458207b552f7ac7a
diegobaena89 <diegobf.cello@gmail.com>
2023-02-15 18:05:30 -0300

    feature: SEO adjusments

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -5,6 +5,7 @@ import { GetServerSideProps } from 'next';
 import Stripe from 'stripe';
 import { stripe } from '../lib/stripe';
 import Image from 'next/image';
+import Head from 'next/head';
 interface SuccessProps {
   customerName: string;
   product: {
@@ -15,20 +16,28 @@ interface SuccessProps {
 
 export default function Success({ customerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | E-commerce shop</title>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt='' />
-      </ImageContainer>
+        <meta name='robots' content='noindex' />
+      </Head>
 
-      <p>
-        Uhuul <strong>{customerName}</strong>, sua{' '}
-        <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
+      <SuccessContainer>
+        <h1>Compra efetuada</h1>
 
-      <Link href='/'>Voltar ao catálogo</Link>
-    </SuccessContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt='' />
+        </ImageContainer>
+
+        <p>
+          Uhuul <strong>{customerName}</strong>, sua{' '}
+          <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
+
+        <Link href='/'>Voltar ao catálogo</Link>
+      </SuccessContainer>
+    </>
   );
 }
 
